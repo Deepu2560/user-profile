26d9d0cdb4b18434a30be24db5bbc227bbc6e060
unknown <deepanshugulia256@gmail.com>
2022-09-21 14:21:28 +0530

    added homepage to backend

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const cors = require("cors");
 // importing connect function which connects from database
 const connect = require("./src/configs/db");
 
-// user controller all required routes
+// user controller all required routes and srver homepage
 const userController = require("./src/Controller/userControl");
+const homePage = require("./serverhomePage");
 
 // app is used for all express function in server I am only using app.use which is mainly used for middleware and app.listen to start server
 const app = express();
@@ -16,6 +17,12 @@ app.use(cors());
 
 // express.json() to get data in json format
 app.use(express.json());
+
+// main / route will return a html page which will tell about backend routes
+app.get("/", (req, res) => {
+  console.log("==> Showing home page");
+  res.status(200).send(homePage);
+});
 
 // route for user
 app.use("/auth", userController);
